b5074f612a25a7e91e9a01409b28654829393e33
grz55 <32485116+grz55@users.noreply.github.com>
2023-11-17 20:58:32 +0100

    feat(60): create pc method implementation (#8)
    * feat(60): create pc method implementation
    
    * feat(60): postman collection added
    
    * fix(60): postman collection names fixes
    
    * fix(60): fetchPCOperatingSystem fetch method renamed

--- src/main/java/com/compstore/service/IPCService.java
+++ src/main/java/com/compstore/service/IPCService.java
@@ -1,9 +1,12 @@
 package com.compstore.service;
 
+import com.compstore.dto.pc.PCCreateRequestDTO;
 import com.compstore.dto.pc.PCDTO;
 import java.util.UUID;
 
 public interface IPCService {
 
     PCDTO getPCById(UUID pcId);
+
+    PCDTO createPC(PCCreateRequestDTO pcCreateRequest);
 }
