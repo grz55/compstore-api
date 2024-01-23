65000c5504b58a1cd9c09bd5090af13a829fa838
grz55 <grz55@vp.pl>
2024-01-24 00:42:06 +0100

    fix(69): cors configuration for profiles

--- src/main/java/com/compstore/config/CrossOriginConfig.java
+++ src/main/java/com/compstore/config/CrossOriginConfig.java
@@ -1,16 +1,32 @@
 package com.compstore.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CrossOriginConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    @Profile("prod")
+    @Configuration
+    public static class ProdCorsConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("https://pawelnu.github.io/compstore-ui/")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+        }
+    }
+
+    @Profile("!prod")
+    @Configuration
+    public static class DevCorsConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:3000")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+        }
     }
 }
