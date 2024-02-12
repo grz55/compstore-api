7f37230e51a8c54ebbc2b9f9911e484e04de5a8c
grz55 <32485116+grz55@users.noreply.github.com>
2024-02-12 09:31:13 +0100

    feat: get processor brands and cors fix
    * feat(36): dockerfile and workflows configuration (#22)
    
    * feat(36): docker and dockerfile configurations, useful commands added
    
    * refactor(36): profiles configuration refactor
    
    * feat(36): github actions workflows configuration
    
    * fix(69): cors configuration for profiles (#21)
    
    * styles: README.md reformat (#24)
    
    * fix: Dockerfile jar name refactor (#27)
    
    * fix(88): spring actuator and maven profiles added (#29)
    
    * fix(88): sql shortened for prod profile
    
    * fix(88): default profile added for running with no profile parameter
    
    * feat(88): actuator endpoints added
    
    * feat(88): allowed frontend as application parameter configured
    
    * fix(88): production profile set for maven and workflows
    
    * fix(88): Dockerfile profiles added, useful docker commands added
    
    * fix(88): Dockerfile commands update
    
    * feat(90): get processor brand
    
    * feat(90): Processor Brand by UUID implemented
    
    * feat(90): Processor brand get by uuid endpoints added, example Render request added
    
    * fix(79): cors allowed frontend null fix, bean creation change
    
    * fix(79): cors allowed frontend value replaced to proper bean
    
    ---------
    
    Co-authored-by: pawelNu <93542936+pawelNu@users.noreply.github.com>

--- src/main/java/com/compstore/config/CrossOriginConfig.java
+++ src/main/java/com/compstore/config/CrossOriginConfig.java
@@ -1,36 +1,41 @@
 package com.compstore.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CrossOriginConfig implements WebMvcConfigurer {
-
-    @Value("${cors.allowed-origins}")
-    private static String allowedFrontend;
+public class CrossOriginConfig {
 
     @Profile("prod")
-    @Configuration
-    public static class ProdCorsConfig implements WebMvcConfigurer {
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                    .allowedOrigins(allowedFrontend)
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-        }
+    @Bean
+    public WebMvcConfigurer prodCorsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Value("${cors.allowed-origins}")
+            private String allowedFrontend;
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedFrontend)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
     }
 
     @Profile("!prod")
-    @Configuration
-    public static class DevCorsConfig implements WebMvcConfigurer {
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-        }
+    @Bean
+    public WebMvcConfigurer noProdCorsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
     }
 }
