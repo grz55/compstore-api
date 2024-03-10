2ce1e7db328d9dc6e71ff973660a264c25b52509
grz55 <grz55@vp.pl>
2024-03-10 16:08:58 +0100

    refactor(43): changed deprecated methods to new

--- src/main/java/com/compstore/config/SecurityConfig.java
+++ src/main/java/com/compstore/config/SecurityConfig.java
@@ -1,21 +1,37 @@
 package com.compstore.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthConverter jwtAuthConverter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeHttpRequests().anyRequest().authenticated();
-        http.oauth2ResourceServer().jwt();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests(customizer -> customizer.anyRequest().authenticated())
+                .oauth2ResourceServer(
+                        httpSecurityOAuth2ResourceServerConfigurer ->
+                                httpSecurityOAuth2ResourceServerConfigurer.jwt(
+                                        jwtConfigurer ->
+                                                jwtConfigurer.jwtAuthenticationConverter(
+                                                        jwtAuthConverter)))
+                .sessionManagement(
+                        httpSecuritySessionManagementConfigurer ->
+                                httpSecuritySessionManagementConfigurer.sessionCreationPolicy(
+                                        SessionCreationPolicy.STATELESS));
         return http.build();
     }
 }
