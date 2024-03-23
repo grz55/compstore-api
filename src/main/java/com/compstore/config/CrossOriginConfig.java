e735d892494af433937e0e75d56d189d4c704acc
grz55 <grz55@vp.pl>
2024-03-23 09:44:34 +0100

    fix(43): default value added to allowed frontend

--- src/main/java/com/compstore/config/CrossOriginConfig.java
+++ src/main/java/com/compstore/config/CrossOriginConfig.java
@@ -10,12 +10,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CrossOriginConfig {
 
+    @Value("${cors.allowed-origins}")
+    private String allowedFrontend;
+
     @Profile("prod")
     @Bean
     public WebMvcConfigurer prodCorsConfigurer() {
         return new WebMvcConfigurer() {
-            @Value("${cors.allowed-origins}")
-            private String allowedFrontend;
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
@@ -33,7 +34,7 @@ public class CrossOriginConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(allowedFrontend)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
